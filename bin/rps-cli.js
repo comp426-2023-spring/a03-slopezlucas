4fc3d648a705e92595b19b80411afdaa011ce031
root <root@DESKTOP-D4RTLKG.localdomain>
2023-05-08 18:39:00 -0400

    added stuff x2

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,40 @@
 #!/usr/bin/env node
+
+import {rps, h, rules} from "../lib/rpsls.js"
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+
+if(args.help || args.h) {
+	h();
+    process.exit(0);
+}
+if(args.r || args.rules) {
+    rules();
+    process.exit(0);
+}
+
+var alength = args._length;
+
+switch(alength) {
+    case alength == 0:
+        console.log(JSON.stringify(rps("null")));
+        process.exit(0);
+    case alength == 1:
+        var player = args._[0].toString().toLowerCase();
+        if(player=="rock" || player=="paper" || player=="scissors"){
+            console.log(JSON.stringify(rps(player)));
+            process.exit(0);
+        } 
+        else {
+            console.log("Unknown input");
+            h();
+            rules();
+            process.exit(0);
+        }
+    default:
+        console.log("Out of range");
+        h();
+        rules();
+        process.exit(0);
+}
